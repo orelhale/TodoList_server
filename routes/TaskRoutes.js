1613c2ab20c1398d83c6f3798bd5e45cf7b25ae4
orelhale <oreljbh@gmail.com>
2023-05-10 20:49:59 +0300

    secondDay

--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -22,7 +22,8 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
    try {
-      res.status(200).send(await servic_addTask(req.body))
+      await servic_addTask(req.body)
+      res.status(200).send(await servic_findAllAndOrder())
    } catch (err) {
       res.status(400).send(err)
    }
@@ -37,7 +38,8 @@ router.put("/", async (req, res) => {
       if (!req.body.id)
          throw ("ERROR: id is emapy")
       await servic_editTask(req.body, req.body.id)
-      res.status(200).send()
+
+      res.status(200).send(await servic_findAllAndOrder())
    } catch (err) {
       res.status(400).send(err)
    }
